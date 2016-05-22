694cc9d5284ed61413ea28f5a52d867bf84f0bd0
Igor Nadj <igor.nadj@gmail.com>
2016-05-22 23:55:33 +1200

    Better reflection on ORM fields

--- code/lib/SSReflection.php
+++ code/lib/SSReflection.php
@@ -12,11 +12,6 @@ class SSReflection {
 	); 
 
 
-	public static $magic_fields = array(
-		'SiteTree' => array(
-			'ParentID' => 'Int', // no idea where this is defined...
-		)
-	);
 
 
 
@@ -44,53 +39,56 @@ class SSReflection {
 	 * @return array of array('name' => string, 'type' => string);
 	 */
 	public static function getDataObjectFields($className){
-		// First we manually add in the base-level fields (ID, Created etc.)
-		$r = array(
-			array(
-				'name' => 'ID', 
-				'humanReadableName' => 'ID', 
-				'type' => 'Int',
-			),
-			array(
-				'name' => 'ClassName', 
-				'humanReadableName' => 'Class Name', 
-				'type' => 'Varchar(255)',
-			),
-			array(
-				'name' => 'RecordClassName', 
-				'humanReadableName' => 'Record Class Name', 
-				'type' => 'Varchar(255)',
-			),
-			array(
-				'name' => 'LastEdited', 
-				'humanReadableName' => 'Last Edited', 
-				'type' => 'SS_Datetime',
-			),
-			array(
-				'name' => 'Created', 
-				'humanReadableName' => 'Created', 
-				'type' => 'SS_Datetime',
-			),
-		);
-		
-		$instance = new $className();
-		/* @var $instance DataObject */
-		
-		// pre-fetch labels (human readable names) for performance
-		$fieldLabelsMap = $instance->fieldLabels();
+		$r = array();
 
-		// get all fields
-		$allFields = $instance->db();
-		$allFields = array_merge($allFields, self::getMagicFields($className));
-
-		// build field spec
-		foreach($allFields as $k => $v){
+		// First we manually add in the base-level fields
+		foreach(DataObject::database_fields($className) as $name => $spec){
 			$r[] = array(
-				'name' => $k, 
-				'humanReadableName' => $fieldLabelsMap[$k],
-				'type' => $v,
+				'name' => $name,
+				'type' => $spec,
+				'humanReadableName' => $name, // TODO
 			);
 		}
+
+		// Then we add inherited fields
+		if($className == 'DataObject'){
+			$always = array(
+				array(
+					'name' => 'ID', 
+					'humanReadableName' => 'ID', 
+					'type' => 'Int',
+				),
+				array(
+					'name' => 'RecordClassName', 
+					'humanReadableName' => 'Record Class Name', 
+					'type' => 'Varchar(255)',
+				),
+			);
+			$r = array_merge($always, $r);
+		}else{
+			$reflectionClass = new ReflectionClass($className);
+			$parentClassName = $reflectionClass->getParentClass()->getName();
+			$r = array_merge($r, self::getDataObjectFields($parentClassName));
+		}
+
+		
+		// $instance = new $className();
+		// /* @var $instance DataObject */
+		
+		// // pre-fetch labels (human readable names) for performance
+		// $fieldLabelsMap = $instance->fieldLabels();
+
+		// // get all fields
+		// $allFields = $instance->db();
+
+		// // build field spec
+		// foreach($allFields as $k => $v){
+		// 	$r[] = array(
+		// 		'name' => $k, 
+		// 		'humanReadableName' => $fieldLabelsMap[$k],
+		// 		'type' => $v,
+		// 	);
+		// }
 		
 		return $r;
 	}
@@ -102,25 +100,6 @@ class SSReflection {
 		}
 		$obj = new $className();
 		return $obj->singular_name();
-	}
-	
-
-	/**
-	  * @return array of name => type
-	  */
-	public static function getMagicFields($className){
-		// direct match against registered magic fields
-		if(isset(self::$magic_fields[$className])){
-			return self::$magic_fields[$className];	
-		}
-		// otherwise if this is a subtype it will have those fields as well
-		$reflectionClass = new ReflectionClass($className);
-		foreach(self::$magic_fields as $magicClass => $magicClassFields){
-			if($reflectionClass->isSubclassOf($magicClass)){
-				return $magicClassFields;
-			}
-		}
-		return array();
 	}
 	
 }
